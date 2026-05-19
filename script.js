f1dd77e9390c1e725bf39a8c6ede6df808a74181
Luis-EGV <luisgonzaga590@gmail.com>
2026-05-19 12:22:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
 
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+let index = 0;
+
+function showSlide() {
+    const slides = document.getElementById('slides');
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    index++;
+
+    if(index > 2){
+        index = 0;
+    }
+
+    showSlide();
+}
+
+function prevSlide() {
+    index--;
+
+    if(index < 0){
+        index = 2;
+    }
+
+    showSlide();
+}
+
+setInterval(nextSlide, 5000);
